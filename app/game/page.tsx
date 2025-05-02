cd0e46d578a9b87106e72b850c057cda3d2f9bc7
TheHackerClown <dhruvprataps14@gmail.com>
2025-05-02 16:10:51 +0530

    Finished Dummy

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,12 +1,22 @@
 "use client";
-import { useEffect, useRef } from "react"
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from "react"
 
 import Hellfire from "/Hellfire.jpg";
 
 export default function Game() {
   const gameref = useRef<HTMLCanvasElement | null>(null);
   const players = ["hehe", "new player", "yo mama", "scrimmy", "cockroachegg", "fat32","teri sasu"]
-  
+
+  const [chats, setChats] = useState<string[]>(["hehe", "new player", "yo mama", "scrimmy", "cockroachegg", "fat32","teri sasu"]);
+
+  const [value, setValue] = useState<string>("");
+
+  const handleval = (e: FormEvent) => {
+    e.preventDefault();
+    setChats([...chats, value])
+    setValue("");
+  }
+
   useEffect(()=>{
     if (gameref.current) {
     const canwidth = gameref.current.width; 
@@ -34,7 +44,7 @@ export default function Game() {
     </h4>
       Players
     <ul>
-      {players.map((val)=>(<li className="m-3 border-2 rounded-xl h-fit text-center">{val} ....</li>))}
+      {players.map((val)=>(<li key={Math.random()*999} className="m-3 border-2 rounded-xl h-fit text-center">{val} ....</li>))}
     </ul>
 
     <div className="border-1 text-center h-full center">
@@ -58,8 +68,23 @@ export default function Game() {
     <div className="text-xs text-center">Made with ❤️ by <a className="text-red-300 hover:border-b-1" href="https://github.com/TheHackerClown">TheHackerClown</a></div>
     </div>
 
-    <div className="border-2 w-99 ml-2 mr-1 mt-1 mb-1 h-dvh">
-    For Global Chat
+    <div className="border-2 text-center w-99 ml-2 mr-1 mt-1 mb-1 h-dvh">
+    Server Chat
+
+    <div className="center border-1 h-10/12 overflow-auto">
+      <ul>
+        {chats.map((val)=>(<li className="m-2 column center text-red-400 border-1 rounded-xl w-full">{val} <small className="text-white">by DHru</small></li>))}
+      </ul>
+      
+    </div>
+    
+    <div className="h-fit p-2 center">
+      <form action="#" className=" row centers" onSubmit={(e)=>handleval(e)} method="post">
+        <input type="text" name="chatpost" className="border-1 mt-auto mb-auto w-full m-4 rounded-xl" id="chatpost" value={value} onChange={(e:ChangeEvent)=>setValue(e.target.value)} />
+        <input type="submit" className="bg-green-600 rounded-xl p-2 text-white" value="Send" />
+      </form>
+    </div>
+    
     </div>
 
   </div>
